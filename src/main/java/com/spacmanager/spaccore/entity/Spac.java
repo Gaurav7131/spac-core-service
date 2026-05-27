3e0b9169ca34733b71eb0a81390babc6d958b58b
Gaurav7131 <thakaregaurav7131@gmail.com>
2026-05-27 17:08:54 +0530

    commit

--- src/main/java/com/spacmanager/spaccore/entity/Spac.java
+++ src/main/java/com/spacmanager/spaccore/entity/Spac.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "spacs")
@@ -22,9 +23,29 @@ public class Spac {
     private String tickerSymbol;
     private String targetSector;
 
-    // Precision 19, Scale 4 is standard for financial systems
+    // precision and scale=4 standard for financial systems
     @Column(precision = 19, scale = 4)
     private BigDecimal trustValue;
 
     private String currentStage;
+
+    // 1. These define the database columns
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // 2. This sets the date automatically right before a row is CREATED
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // 3. This updates the date automatically right before a row is UPDATED
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
